9b3c769be27d956680d08061c1f81828ccc63282
benedict-lq <benedictpadronzulan@gmail.com>
2026-09-02 11:44:42 +0800

    Add files via upload

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,51 +1,40 @@
-money = 15428
-print("Money to Deposit -->", money)
+print()
+print("========================= CASH-G Bank DENOMINTION =========================")
+print()
+money = eval(input("Enter the amount of money to deposit-----> "))
+print()
+print("Money to Deposit -------->", money)
 
-a = 1000
-b = 500
-c = 200
-d = 100
-e = 50
-f = 20
-g = 10
-h = 5
-i = 1
+a1 = money // 1000
+change1 = money % 1000
+print("1000 PHP --> ", a1)
 
-a1 = money // a
-money = money % a
-print("1000:", a1)
+a2 = change1 // 500
+change2 = change1 % 500
+print("500 PHP --> ", a2)
 
-a2 = money // b
-money = money % b
-print("500:", a2)
+a3 = change2 // 100
+change3 = change2 % 100
+print("100 PHP --> ", a3)
 
-a3 = money // c
-money = money % c
-print("200:", a3)
+a4 = change3 // 50
+change4 = change3 % 50
+print("50 PHP --> ", a4)
 
-a4 = money // d
-money = money % d
-print("100:", a4)
+a5 = change4 // 20
+change5 = change4 % 20
+print("20 PHP --> ", a5)
 
-a5 = money // e
-money = money % e
-print("50:", a5)
+a6 = change5 // 10
+change6 = change5 % 10
+print("10 PHP --> ", a6)
 
-a6 = money // f
-money = money % f
-print("20:", a6)
+a7 = change6 // 5
+change7 = change6 % 5
+print("5 PHP --> ", a7)
 
-a7 = money // g
-money = money % g
-print("10:", a7)
-
-a8 = money // h
-money = money % h
-print("5:", a8)
-
-a9 = money // i
-money = money % i
-print("1:", a9)
-
-sum = a1 * a + a2 * b + a3 * c + a4 * d + a5 * e + a6 * f + a7 * g + a8 * h + a9 * i
-print("Total Money to Deposit -->", sum)
+a8 = change7 // 1
+change8 = change7 % 1
+print("1 PHP --> ", a8)
+print()
+print("========================= END OF DEPOPSIT =========================")
