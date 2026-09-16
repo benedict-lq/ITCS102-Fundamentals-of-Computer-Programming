7e65cc7458797c3a6a9705a37e1d70c510830048
benedict-lq <benedictpadronzulan@gmail.com>
2026-09-16 11:26:55 +0800

    Add files via upload

---
+++ codechallenge4.py
@@ -0,0 +1,56 @@
+print("======= LOGIN =======")
+
+u = "Benedict"
+p = "akolangto123"
+
+username = input("Please enter username --->: ")
+password = input("Please enter password --->: ")
+
+if u == username and p == password:
+    
+
+
+
+loanee_fname_job_description = input("Enter your first name and your job description")
+
+
+
+
+
+# Inputs:
+age = int(input("Enter your age ---->: "))
+is_employed = bool(input("Are you employed? (yes/no) ---->: ") == "yes")
+credit_score = int(input("Enter your credit score ---->: "))
+annual_income = float(input("Enter your annual income ---->: "))
+has_collateral = bool(input("Do you have collateral? (yes/no) ---->: ") == "yes")
+
+
+if age >= 21 and is_employed == True:
+    if credit_score >= 750:
+        print(f"Your credit score is: {credit_score}")
+        if annual_income >= 100000:
+            print("You have high annual income") 
+            base_interest = 4.5
+            print(f"Approved at {base_interest} interest rate")
+        else:
+            base_interest = 5.0
+            print(f"Approved at {base_interest} interest rate")
+
+    elif 600 <= credit_score < 750:
+        print(f"Your credit score is: {credit_score}")
+        if has_collateral == True:
+            print("You have a collateral")
+            base_interest = 7.0
+            print(f"Approved at {base_interest} interest rate")
+        elif annual_income < 40000:
+            base_interest = 9.5
+            print(f"Approved at {base_interest} interest rate")
+        else:
+            base_interest = 8.0
+            print(f"Approved at {base_interest} interest rate")
+
+    elif credit_score < 600:
+        print("Rejected: Credit score too low")
+
+else:
+    print("Rejected: Fails baseline ")
